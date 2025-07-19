337edc9644c29e5db1a3beaaa897f2cf993b865f
mkslge <marklseeliger@gmail.com>
2025-07-19 07:52:40 -0400

    started lexer

---
+++ src/main/java/Lexer.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+public class Lexer {
+    private String input;
+    private String [] splitInput;
+    private List<Token> tokenizedOutput;
+
+    public Lexer(String input) {
+        this.input = input;
+        this.lex();
+    }
+
+    private void lex() {
+
+    }
+
+
+}
